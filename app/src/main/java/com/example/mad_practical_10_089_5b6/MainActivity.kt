43b771cbc2939206bf2ba5e33d1a5230696a2edb
Admin <Admin@123gmail.com>
2023-10-26 12:35:59 +0530

    initial

--- app/src/main/java/com/example/mad_practical_10_089_5b6/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_089_5b6/MainActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val imageid= arrayOf<Int>(
+        R.drawable.baseline_person_24,R.drawable.baseline_location_on_24,
+        R.drawable.baseline_person_24,R.drawable.baseline_location_on_24,
+        R.drawable.baseline_person_24,R.drawable.baseline_location_on_24,
+        R.drawable.baseline_person_24,R.drawable.baseline_location_on_24,
+        R.drawable.baseline_person_24,R.drawable.baseline_location_on_24,
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.location_list)
+        setContentView(R.layout.activity_main)
+
+//        val myListAdapter = MyListAdapter()
     }
 }
