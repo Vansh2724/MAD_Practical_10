81605d0045f991a85c4306c64d5918be827017f5
Mo_husen <mhusenhaji0@gmail.com>
2023-11-02 12:44:09 +0530

    initial

--- app/src/main/java/com/example/mad_practical_10_089_5b6/RecyclerPersonlistAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_089_5b6/RecyclerPersonlistAdapter.kt
@@ -5,50 +5,46 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.MultiAutoCompleteTextView.CommaTokenizer
+import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
-import java.io.Serializable
 
- class RecyclerPersonlistAdapter : RecyclerView.Adapter<RecyclerPersonlistAdapter.ViewHolder> {
-    private var context:Context
-    private var arr_records = ArrayList<LocationlistCardModel>()
-
-    constructor(context: Context,arr_records:ArrayList<LocationlistCardModel>){
-        this.context =context
-        this.arr_records=arr_records
+class RecyclerPersonlistAdapter: RecyclerView.Adapter<RecyclerPersonlistAdapter.ViewHolder> {
+    private var context: Context
+    private var arr_records = ArrayList<PersonListCardModel>()
+    constructor(context: Context, arr_records: ArrayList<PersonListCardModel>){
+        this.context = context
+        this.arr_records = arr_records
     }
-
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-
-        val txt_name : TextView = itemView.findViewById(R.id.txtview1)
-        val txt_email : TextView = itemView.findViewById(R.id.txtview2)
-        val txt_mobno : TextView = itemView.findViewById(R.id.txtview3)
-        val txt_address : TextView = itemView.findViewById(R.id.txtview4)
-        val btn_map : Button = itemView.findViewById(R.id.btn_map)
-
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txt_name: TextView = itemView.findViewById(R.id.txt_name)
+        val txt_email: TextView = itemView.findViewById(R.id.txt_email)
+        val txt_mobno: TextView = itemView.findViewById(R.id.txt_mobno)
+        val txt_address: TextView = itemView.findViewById(R.id.txt_address)
+        val btn_map: ImageButton = itemView.findViewById(R.id.btn_map)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.location_list,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_personlist, parent, false)
         return ViewHolder(view)
     }
 
-     override fun getItemCount(): Int {
-         return arr_records.size
-     }
-
-     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.txt_name.text = arr_records[position].name
-        holder.txt_email.text = arr_records[position].email
-        holder.txt_mobno.text = arr_records[position].mobno
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txt_name.text = arr_records[position].name
+        holder.txt_email.text = arr_records[position].emailld
+        holder.txt_mobno.text = arr_records[position].phoneNo
         holder.txt_address.text = arr_records[position].address
 
-//        var obj = this as Serializable
-//        holder.btn_map.setOnClickListener{
-//            Intent(this@RecyclerPersonlistAdapter.context,Map)
-//        }
-     }
+        val selectedItem = arr_records[position]
+        holder.btn_map.setOnClickListener {
+            Intent (this@RecyclerPersonlistAdapter.context, MapsActivity::class.java).apply {
+                putExtra("Object",selectedItem)
+                this@RecyclerPersonlistAdapter.context.startActivity( this)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return arr_records.size
+    }
 }
